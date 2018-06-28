b0634303a9d9a0cd9ec3861977257c145cb51d59
Amulya Kumar Sahoo <amulya349@gmail.com>
2018-06-28 13:02:53 +0530

    Fix issue with application variable in wsgi.py
    By default, the application variable is all small by default and is seldom changed. It might have been capitalized by pylint.
    Due to this, Gunicorn was unable to find the application variable. The variable name is now reverted back to small letters.
    
    Referenced issue: #35

--- backend/codexpose/codexpose/settings.py
+++ backend/codexpose/codexpose/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'codexpose.wsgi.APPLICATION'
+WSGI_APPLICATION = 'codexpose.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
